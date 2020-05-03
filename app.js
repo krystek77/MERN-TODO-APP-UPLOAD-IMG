4b6213195809ecd85c333dd815159a95c5d38539
krystek77 <krystian.wrona77@gmail.com>
2020-05-03 10:39:05 +0200

    Add path to images

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 //
 const app = express();
 //
 
 app.use(
-	cors({
-		origin: '*',
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		preflightContinue: false,
-		optionsSuccessStatus: 204,
-	})
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
 );
-app.use(morgan('dev'));
-app.use('/uploads', express.static('uploads'));
-const userRoutes = require('./api/routes/users');
-const taskRoutes = require('./api/routes/tasks');
+app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("uploads"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "uploads"));
+  });
+}
+const userRoutes = require("./api/routes/users");
+const taskRoutes = require("./api/routes/tasks");
 
 //
 
 app.use(bodyParser.json());
 //Routes
-app.use('/users', userRoutes);
-app.use('/tasks', taskRoutes);
+app.use("/users", userRoutes);
+app.use("/tasks", taskRoutes);
 
 //Serve static files if in production version
 
-if (process.env.NODE_ENV === 'production') {
-	//Set static folder
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 //Handle errors
 app.use((req, res, next) => {
-	const error = new Error('NOT FOUNDED');
-	error.status = 404;
-	next(error);
+  const error = new Error("NOT FOUNDED");
+  error.status = 404;
+  next(error);
 });
 app.use((error, req, res, next) => {
-	res.status(error.status || 500 || 401);
-	res.json({ message: error.message, status: error.status });
+  res.status(error.status || 500 || 401);
+  res.json({ message: error.message, status: error.status });
 });
 module.exports = app;
