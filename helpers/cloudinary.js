c4db6976dbc43b3d706246b9e096626cfa8a0a7c
krystek77 <krystian.wrona77@gmail.com>
2020-05-08 12:36:30 +0200

    remove cloudinary.js

--- helpers/cloudinary.js
+++
@@ -1,6 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-module.exports = cloudinary.config({
-  cloud_name: "doydwvtkw",
-  api_key: "874837483274837",
-  api_secret: "9hYGUFEFVLwchVSnPKkC2wWBsFA",
-});
