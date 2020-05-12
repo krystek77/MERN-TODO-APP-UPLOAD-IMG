27d72e20026db8812b4a56c060644491c68962e7
krystek77 <krystian.wrona77@gmail.com>
2020-05-12 10:46:36 +0200

    Update loadingSpinner component
    Adding description props and describing prop type and default prop value

--- client/src/components/shared/LoadingSpinner/LoadingSpinner.js
+++ client/src/components/shared/LoadingSpinner/LoadingSpinner.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import Spinner from 'react-spinkit';
-import teal from '@material-ui/core/colors/teal';
-import {Typography} from '@material-ui/core';
+import React from "react";
+import PropTypes from "prop-types";
+import Spinner from "react-spinkit";
+import teal from "@material-ui/core/colors/teal";
+import { Typography } from "@material-ui/core";
 
-function LoadingSpinner() {
-	return (
-        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-            <Typography component='h1' variant='h3'>Loading...</Typography>
-            <Spinner name="pacman" color={teal[100]} />
-        </div>
-    );
+function LoadingSpinner({ description }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        component="h1"
+        variant="h3"
+      >{`${description} ...`}</Typography>
+      <Spinner name="pacman" color={teal[100]} />
+    </div>
+  );
 }
+LoadingSpinner.defaultProps = {
+  description: "Loading",
+};
+LoadingSpinner.propTypes = {
+  description: PropTypes.string,
+};
 export default LoadingSpinner;
